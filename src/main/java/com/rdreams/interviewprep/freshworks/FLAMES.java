f1e91c35eb1052e787612e87f4366de8f464b716
Roboticsdreams <p.rathinavel@gmail.com>
2024-02-10 13:22:46 +0530

    20240210

--- src/main/java/com/rdreams/interviewprep/freshworks/FLAMES.java
+++ src/main/java/com/rdreams/interviewprep/freshworks/FLAMES.java
@@ -11,35 +11,30 @@ Output: F
  */
 public class FLAMES {
     public String getFLAMES(String name1, String name2) {
-        char[] flames = new char[]{'F','L','A','M','E','S'};
         char[] charname1 = name1.toCharArray();
         char[] charname2 = name2.toCharArray();
         Arrays.sort(charname1);
         Arrays.sort(charname2);
         List<Character> chars = getIntersection(charname1,charname2);
-        int rc = chars.size();
-        int fc = flames.length-1;
-        int l = 1, i = 0;
-        while (i >= 0) {
-            if (l==rc) {
-                for (int k = i; k < flames.length-1; k++) {
-                    flames[k] = flames[k + 1];
-                }
-                flames[flames.length-1] = '0';
-                fc--;
-                i--;
-                l = 0;
+        int charcount = chars.size();
+
+        String flames = "FLAMES";
+        StringBuilder sb = new StringBuilder(flames);
+
+        while(sb.length()!=1)
+        {
+            int val = charcount % sb.length();
+            String temp;
+
+            if(val!=0) {
+                temp = sb.substring(val)+sb.substring(0, val-1);
             }
-            if (i==fc) {
-                i = -1;
+            else {
+                temp = sb.substring(0, sb.length()-1);
             }
-            if(fc == 0) {
-                break;
-            }
-            l++;
-            i++;
+            sb = new StringBuilder(temp);
         }
-        return flames[0]+"";
+        return sb.toString();
     }
 
     public List<Character> getIntersection(char[] name1, char[] name2) {
